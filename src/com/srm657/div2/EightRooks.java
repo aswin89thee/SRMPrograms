26b0184e9710c0260bc6f179a8cbcca344a45fe1
Aswin Rajamannar <aswin89thee@gmail.com>
2015-05-12 20:47:29 -0400

    Changed package name to follow convention

---
+++ src/com/srm657/div2/EightRooks.java
@@ -0,0 +1,33 @@
+package com.srm657.div2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EightRooks {
+	
+	public String isCorrect(String[] board){
+		int rookCount = 0;
+		String correct = "Correct";
+		String incorrect = "Incorrect";
+		int i,j;
+		for(i =0 ; i < board.length ; i++){
+			for(j = 0 ; j < board[i].length(); j++){
+				if(board[i].charAt(j) == 'R')
+					rookCount++;
+			}
+		}
+		if(rookCount != 8) return incorrect;
+		List<Integer> rowRooks = new ArrayList<Integer>();
+		List<Integer> colRooks = new ArrayList<Integer>();
+		for(i = 0 ; i < board.length; i++){
+			for(j = 0 ; j < board[i].length() ; j++){
+				if(board[i].charAt(j) == 'R'){
+					if(rowRooks.contains(i) || colRooks.contains(j)) return incorrect;
+					rowRooks.add(i); colRooks.add(j);
+				}
+			}
+		}
+		return correct;
+	}
+
+}
